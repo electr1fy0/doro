7c52e84d82e42c439f759dbabf67f07b5f07dbcc
electr1fy0 <electr1fy0@icloud.com>
2025-08-01 20:34:42 +0530

    feat: add stopwatch, fix: use \r for printing to overrite current time

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ import (
 
 var cmd = &cobra.Command{
 	Use:   "doro",
-	Short: "a cli pomodoro tool",
+	Short: "a cli clock tool",
 }
 
-var startCmd = &cobra.Command{
-	Use:   "start [duration]",
-	Short: "start a new timer e.g. doro new 30m",
+var startTimerCmd = &cobra.Command{
+	Use:   "timer [duration]",
+	Short: "start a timer e.g. doro timer 30m",
 	Run: func(cmd *cobra.Command, args []string) {
 		startTimer(args[0])
 	},
 	Args: cobra.ExactArgs(1),
+}
+
+var startStopwatchCmd = &cobra.Command{
+	Use:   "stopwatch",
+	Short: "start a stopwatch",
+	Run: func(cmd *cobra.Command, args []string) {
+		startStopWatch()
+	},
 }
 
 func startTimer(duration string) {
@@ -35,16 +43,32 @@ func startTimer(duration string) {
 			remaining := time.Until(end)
 			remaining = max(0, remaining)
 
-			fmt.Println("Time left: ", remaining.Truncate(time.Second))
+			fmt.Printf("\rTime left: %-5v", remaining.Truncate(time.Second))
 		case <-timer.C:
 			fmt.Println("Timer is up")
 			return
 		}
 	}
+}
 
+func startStopWatch() {
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+
+	start := time.Now()
+	fmt.Println("Stopwatch is running... (<ctrl-c> to stop)")
+
+	for {
+		<-ticker.C
+		elapsed := time.Since(start)
+
+		fmt.Printf("\rElapsed: %-8v", elapsed.Truncate(time.Millisecond))
+	}
 }
 
 func main() {
-	cmd.AddCommand(startCmd)
+	cmd.AddCommand(startTimerCmd)
+	cmd.AddCommand(startStopwatchCmd)
+
 	cmd.Execute()
 }
